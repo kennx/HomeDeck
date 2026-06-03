9f8af2266fd8fb26329b8f9e81c941546bf5f581
Kenn <zhangxjian@me.com>
2026-06-03 11:17:35 +0800

    refactor: remove FT_LOAD_TARGET_MONO from font_to_vlw encoder
    - Remove FT_LOAD_TARGET_MONO flag from FreeType character loading to allow 8bpp grayscale rendering.

--- tools/font_to_vlw.cpp
+++ tools/font_to_vlw.cpp
@@ -149,8 +149,7 @@ std::vector<std::uint8_t> copyBitmap(const FT_Bitmap& bitmap) {
 }
 
 Glyph renderGlyph(FT_Face face, std::uint32_t code_point) {
-  if (FT_Load_Char(face, code_point, FT_LOAD_RENDER | FT_LOAD_TARGET_MONO) !=
-      0) {
+  if (FT_Load_Char(face, code_point, FT_LOAD_RENDER) != 0) {
     std::ostringstream message;
     message << "failed to render U+" << std::uppercase << std::hex
             << code_point;
